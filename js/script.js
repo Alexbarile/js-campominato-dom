afee2914826f8e1ea40f41ee13b40d5a54ae3c2a
“Alexbarile” <alexbarile93fg@yahoo.it>
2022-12-07 13:24:40 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,44 @@
+
+// creo un div con una classe specifica
+
+function createGrid(number){
+    const element = document.createElement('div');
+    element.classList.add('square');
+    element.innerText = number;
+    return element;
+    
+}
+
+// creo e definisco il pulsante play
+
+let button = document.getElementById('button');
+button.addEventListener('click', function(){
+
+    // richiamo la funzione
+
+    let grid = document.getElementById('grid');
+    
+    // non faccio ripetere la griglia ogni volta che schiaccio play
+
+    if(grid){
+        grid.innerHTML = '';
+    }
+
+    // creo il ciclo for con i numeri
+
+    for(let i=0; i<100; i++){
+        const square = createGrid(i+1);
+        
+        // aggiungo la funzione quando schiaccio sulla casella cambia colore
+    
+        square.addEventListener('click', function(){
+            this.classList.toggle('clicked');
+            console.log(`Hai selezionato il numero ${this.innerText}`)
+        });
+    
+        // creo un figlio di grid
+
+        grid.appendChild(square);
+    }
+
+});
